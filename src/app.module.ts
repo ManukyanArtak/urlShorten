6b01b0c586ff7cbc639175babb72ca63ed50bfdc
artak_manukyan <artakm@tcf.team>
2025-07-06 16:00:26 +0400

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { User } from './users/user.entity';
 import { UrlModule } from './url/url.module';
 import { ShortUrl } from './url/url.entity';
 import { AuthModule } from './auth/auth.module';
-import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { EventEmitterModule } from '@nestjs/event-emitter';
@@ -44,7 +42,7 @@ import { IsUnique } from './common/validation/is-unique.constraint';
     AuthModule,
     UrlModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, IsUnique],
+  controllers: [],
+  providers: [IsUnique],
 })
 export class AppModule {}
